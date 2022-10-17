530ccc16d8b7194994388db4b75d9010d5f81541
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-10-17 17:29:57 +0300

    done hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
@@ -8,6 +8,8 @@ export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [positivePercentage, setPositivePercentage] = useState(0);
 
   const handleClick = name => {
     switch (name) {
@@ -24,14 +26,16 @@ export default function App() {
         return;
     }
   };
-  const countTotalFeedback = () => {
-    const total = good + neutral + bad;
-    return total;
-  };
-  const countPositiveFeedbackPercentage = () => {
-    const positive = (good / countTotalFeedback()) * 100;
-    return positive ? Math.round(positive) : 0;
-  };
+
+  useEffect(() => {
+    setTotal(good + neutral + bad);
+  }, [good, neutral, bad]);
+
+  useEffect(() => {
+    const positive = (good / total) * 100;
+    setPositivePercentage(positive ? Math.round(positive) : 0);
+  }, [total, good]);
+
   return (
     <Section title="Please leave feedback">
       <FeedbackOptions
@@ -45,8 +49,8 @@ export default function App() {
           good={good}
           neutral={neutral}
           bad={bad}
-          total={countTotalFeedback()}
-          positivePercentage={countPositiveFeedbackPercentage()}
+          total={total}
+          positivePercentage={positivePercentage}
         />
       )}
     </Section>
